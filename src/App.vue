6994b9aef90f9d92dc09c894bee4db7991f4d5dc
kdjordan <k.dean.jordan@gmail.com>
2024-09-18 15:05:56 -0700

    REF: changing branch

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@
       </div>
     </div>
     <span class="ml-[300px]">{{ userProfileInfo }}</span> 
+    {{ DBstore }}
   </div>
 </template>
 
@@ -46,7 +47,7 @@ onMounted(async () => {
     // Initialize with dummy data and sample DB
     // First parameter: isPaid (true for paid user, false for free user)
     // Second parameter: loadSampleDB (true to load sample data, false to skip)
-    await initAppWithSampleData(true, true);
+    await initAppWithSampleData(true, false);
     console.log('App initialized with sample data');
     
     // Add event listener for beforeunload
