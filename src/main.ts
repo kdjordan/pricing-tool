b11a70bfee37f94a859d07b03c134007449cc219
kdjordan <k.dean.jordan@gmail.com>
2024-10-09 20:15:18 -0700

    REF : adding vuetify - kinda working

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,30 @@
 import './assets/index.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+// Import Vuetify styles and Material Design Icons
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css' 
+
+
 // @ts-ignore
 import App from './App.vue'
 import router from './router'
 
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi', // Use MDI as the default icon set
+  },
+})
+
 const app = createApp(App)
 const pinia = createPinia()
 
-app.use(router).use(pinia)
+app.use(router).use(pinia).use(vuetify)
 
 app.mount('#app')
